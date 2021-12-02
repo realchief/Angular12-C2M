5a678d1b078a5432142e72cac45035077aadf877
realchief <stryokka@gmail.com>
2021-12-01 20:30:52 -0500

    built a div for slidebar company search

--- src/app/layouts/menu-right/menu-right.component.ts
+++ src/app/layouts/menu-right/menu-right.component.ts
@@ -4,6 +4,7 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 @Component({
     selector: "app-menu-right",
     templateUrl: "./menu-right.component.html",
+    styleUrls: ['./menu-right.component.css'],
     encapsulation: ViewEncapsulation.None,
 })
 export class MenuRightComponent implements OnInit {
