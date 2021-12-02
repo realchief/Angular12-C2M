0da71644e5e5acade45d2b10f20ee85471699d1f
realchief <stryokka@gmail.com>
2021-12-01 22:39:46 -0500

    modal when click add company button

--- src/app/layouts/menu-right/menu-right.component.ts
+++ src/app/layouts/menu-right/menu-right.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
 
 
 @Component({
@@ -9,12 +10,52 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 })
 export class MenuRightComponent implements OnInit {
 
+    companynames = [
+        'Ant-Man',
+        'Aquaman',
+        'Asterix',
+        'The Atom',
+        'The Avengers',
+        'Batgirl',
+        'Batman',
+        'Batwoman'
+    ]
 
-    constructor() {
-
+    closeResult: string = '';
+    constructor(
+        private modalService: NgbModal
+    ) {
     }
     ngOnInit() {
 
+    }
+    uploadPicture() {
+        debugger;
+        const imgInt = <HTMLInputElement>document.getElementById('company-image');
+        const file = imgInt.files
+        if (file) {
+            const previewEle = <HTMLImageElement>document.getElementById('preview-show');
+            previewEle.src = URL.createObjectURL(file[0])
+        }
+
+    }
+
+    open(content: any) {
+        this.modalService.open(content, { ariaLabelledBy: 'modal-basic-title' }).result.then((result) => {
+            this.closeResult = `Closed with: ${result}`;
+        }, (reason) => {
+            this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+        });
+    }
+
+    private getDismissReason(reason: any): string {
+        if (reason === ModalDismissReasons.ESC) {
+            return 'by pressing ESC';
+        } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+            return 'by clicking on a backdrop';
+        } else {
+            return `with: ${reason}`;
+        }
     }
 
 }
--- src/app/modules/data-interface/interface-grid/interface-grid.component.ts
+++ src/app/modules/data-interface/interface-grid/interface-grid.component.ts
@@ -5,81 +5,81 @@ import { FormControl } from '@angular/forms';
 import { map, debounceTime, distinctUntilChanged } from 'rxjs/operators';
 import { ApiService } from 'src/app/_services/api.service';
 import { Title } from '@angular/platform-browser';
-import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
+import { NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
 
 
 
 @Component({
-    selector: 'app-interface-grid',
-    templateUrl: './interface-grid.component.html'
+  selector: 'app-interface-grid',
+  templateUrl: './interface-grid.component.html'
 })
 export class InterfaceGridComponent implements OnInit, OnDestroy {
 
-    dataSource: any;
-    itemsCount = 0;
-    closeResult: string = '';
+  dataSource: any;
+  itemsCount = 0;
+  closeResult: string = '';
 
 
-    constructor(
-        private router: Router,
-        private activeRoute: ActivatedRoute,
-        private apiService: ApiService,
-        private titleService: Title,
-        private modalService: NgbModal
-    ) {
-        sessionStorage.setItem('AppTitle', 'Data Interface');
+  constructor(
+    private router: Router,
+    private activeRoute: ActivatedRoute,
+    private apiService: ApiService,
+    private titleService: Title,
+    private modalService: NgbModal
+  ) {
+    sessionStorage.setItem('AppTitle', 'Data Interface');
+  }
+
+  ngOnInit() {
+    this.titleService.setTitle('ONE | Data Interface');
+    this.getAllCompany();
+  }
+
+
+  private getAllCompany() {
+    // const apikey = 'bIPXlfzvB1kHilurK4s@jjnOiDCoVQ';
+    const apikey = localStorage.getItem("APIKey");
+    this.apiService.get('GetCompanies', { 'apiKey': apikey, 'companyname': '' })
+      .subscribe(res => {
+        this.dataSource = res.data.companies.company;
+        console.log(this.dataSource);
+      }, error => {
+        console.log(error);
+      });
+
+  }
+
+  ngOnDestroy() {
+    sessionStorage.removeItem('AppTitle');
+  }
+
+  uploadPicture() {
+    debugger;
+    const imgInt = <HTMLInputElement>document.getElementById('company-image');
+    const file = imgInt.files
+    if (file) {
+      const previewEle = <HTMLImageElement>document.getElementById('preview-show');
+      previewEle.src = URL.createObjectURL(file[0])
     }
 
-    ngOnInit() {
-        this.titleService.setTitle('ONE | Data Interface');
-        this.getAllCompany();
+  }
+
+  open(content: any) {
+    this.modalService.open(content, { ariaLabelledBy: 'modal-basic-title' }).result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
+    }, (reason) => {
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    });
+  }
+
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return `with: ${reason}`;
     }
-
-
-    private getAllCompany() {
-        // const apikey = 'bIPXlfzvB1kHilurK4s@jjnOiDCoVQ';
-        const apikey = localStorage.getItem("APIKey");
-        this.apiService.get('GetCompanies', {'apiKey': apikey, 'companyname': ''})
-            .subscribe(res => {                
-                this.dataSource = res.data.companies.company;
-                console.log(this.dataSource);
-            }, error => {
-                console.log(error);
-            });
-
-    }
-
-    ngOnDestroy() {
-        sessionStorage.removeItem('AppTitle');
-    }
-
-    uploadPicture() {
-      debugger;
-        const imgInt = <HTMLInputElement> document.getElementById('company-image');
-        const file = imgInt.files
-        if (file) {
-            const previewEle = <HTMLImageElement>document.getElementById('preview-show');
-            previewEle.src = URL.createObjectURL(file[0])
-        }
-
-    }
-
-    open(content:any) {
-        this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
-          this.closeResult = `Closed with: ${result}`;
-        }, (reason) => {
-          this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
-        });
-      }
-      
-      private getDismissReason(reason: any): string {
-        if (reason === ModalDismissReasons.ESC) {
-          return 'by pressing ESC';
-        } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
-          return 'by clicking on a backdrop';
-        } else {
-          return  `with: ${reason}`;
-        }
-      }
+  }
 
 }
