098e361c82f3fb2744b2ca025cdd0b68443eb6fd
realchief <stryokka@gmail.com>
2021-12-08 20:45:37 -0500

    finished validation of all forms for creating a channel

--- src/app/modules/data-interface/channel-add/channel-add.component.ts
+++ src/app/modules/data-interface/channel-add/channel-add.component.ts
@@ -90,13 +90,13 @@ export class ChannelAddComponent implements OnInit, OnDestroy {
     this.addChannelForm = this.formBuilder.group({
       channel_name: ['', Validators.required],
       permission: [''],
-      channel_type: ['', Validators.required],
-      ttl_period: ['', Validators.required],
-      channel_description: ['', Validators.required],
-      channel_tags: ['', Validators.required],
-      navigation_tab: ['', Validators.required],
-      channel_ttl_rate: ['', Validators.required],
-      channel_navigation_tab: ['', Validators.required],
+      channel_type: [''],
+      ttl_period: [''],
+      channel_description: [''],
+      channel_tags: [''],
+      navigation_tab: [''],
+      channel_ttl_rate: [''],
+      channel_navigation_tab: [''],
     });
   }
 
@@ -150,37 +150,7 @@ export class ChannelAddComponent implements OnInit, OnDestroy {
 
   onSubmit(): void {
     this.submitted = true;
-    let form_data_payload = {
-      CountryId: this.f.country.value,
-      PolicyBundleIdntryId: this.f.policy_bundle.value,
-      ZipCode: this.f.zip_code.value,
-      Address1: this.f.address1.value,
-      Address2: this.f.address2.value,
-      StateId: this.f.state.value,
-      WebURL: this.f.company_url.value,
-      city: this.f.city.value,
-      emailaddress: this.f.emailaddress.value,
-      CompanyName: this.f.company_name.value,
-      Phone: this.f.phone.value,
-      CategoryIds: '13,15',
-      Description: 'test',
-      Domain: '12345678975'
-    }
-      this.apiService.postWithApiKey('v1/AddCompany', form_data_payload).subscribe(
-      data => {
-        console.log(data);
-        if (data.status == 'SUCCESS') {
-          this.router.navigate(['/company']);
-        } else {
-          this.errorMessage = data.data.response;
-          this.isCreatingFailed = true;
-        }
-      },
-      err => {
-        this.errorMessage = err.error.message;
-        this.isCreatingFailed = true;
-      }
-    );
+    console.log('submitted');
   }
 
   reloadPage(): void {
