975e617569b322b9df615e4df9713a6e1e7143a8
realchief <stryokka@gmail.com>
2021-12-12 22:39:22 -0500

    url validation in file channel wizard

--- src/app/modules/data-interface/file-channel-add/file-channel-add.component.ts
+++ src/app/modules/data-interface/file-channel-add/file-channel-add.component.ts
@@ -103,6 +103,7 @@ export class FileChannelAddComponent implements OnInit, OnDestroy {
   ) {
     sessionStorage.setItem('AppTitle', 'Add Channel');
     this.getCountries();
+    const reg = '(https?://)?([\\da-z.-]+)\\.([a-z.]{2,6})[/\\w .-]*/?';
     this.addChannelForm = this.formBuilder.group({
       channel_name: ['', Validators.required],
       permission: [''],
@@ -113,9 +114,9 @@ export class FileChannelAddComponent implements OnInit, OnDestroy {
       channel_ttl_rate: [''],
       channel_navigation_tab: [''],
       image: ['', Validators.required],
-      rss_feed_url: [''],
-      buy_it_url: [''],
-      subscribe_url: ['']
+      rss_feed_url: ['', [Validators.pattern(reg)]],
+      buy_it_url: ['', [Validators.pattern(reg)]],
+      subscribe_url: ['', [Validators.pattern(reg)]]
     });
   }
 
