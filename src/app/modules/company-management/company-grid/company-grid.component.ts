59225ac4a0b1e10dfe2eaad4f55f400c553b77a8
realchief <stryokka@gmail.com>
2022-02-03 07:54:55 -0500

    reverted some codes for material ui

--- src/app/modules/company-management/company-grid/company-grid.component.ts
+++ src/app/modules/company-management/company-grid/company-grid.component.ts
@@ -1,33 +1,30 @@
 import { Component, OnInit, OnDestroy, ViewChild } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-import { DatePipe, formatDate } from '@angular/common';
-import { FormControl } from '@angular/forms';
-import { map, debounceTime, distinctUntilChanged } from 'rxjs/operators';
 import { ApiService } from 'src/app/_services/api.service';
 import { Title } from '@angular/platform-browser';
-import { MatPaginator } from '@angular/material/paginator';
-import { MatTableDataSource } from '@angular/material/table';
+// import { MatPaginator } from '@angular/material/paginator';
+// import { MatTableDataSource } from '@angular/material/table';
 
 
-export interface PeriodicElement {
-    name: string;
-    position: number;
-    weight: number;
-    symbol: string;
-}
+// export interface PeriodicElement {
+//     name: string;
+//     position: number;
+//     weight: number;
+//     symbol: string;
+// }
 
-const ELEMENT_DATA: PeriodicElement[] = [
-    { position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H' },
-    { position: 2, name: 'Helium', weight: 4.0026, symbol: 'He' },
-    { position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li' },
-    { position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be' },
-    { position: 5, name: 'Boron', weight: 10.811, symbol: 'B' },
-    { position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C' },
-    { position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N' },
-    { position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O' },
-    { position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F' },
-    { position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne' },
-];
+// const ELEMENT_DATA: PeriodicElement[] = [
+//     { position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H' },
+//     { position: 2, name: 'Helium', weight: 4.0026, symbol: 'He' },
+//     { position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li' },
+//     { position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be' },
+//     { position: 5, name: 'Boron', weight: 10.811, symbol: 'B' },
+//     { position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C' },
+//     { position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N' },
+//     { position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O' },
+//     { position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F' },
+//     { position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne' },
+// ];
 
 
 
@@ -40,16 +37,15 @@ export class CompanyGridComponent implements OnInit, OnDestroy {
     companyInfoList: any;
     itemsCount = 0;
 
-    displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
-    dataSource = new MatTableDataSource<PeriodicElement>(ELEMENT_DATA);
-
-    @ViewChild(MatPaginator) paginator: MatPaginator;
+    // displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
+    // dataSource = new MatTableDataSource<PeriodicElement>(ELEMENT_DATA);
 
     constructor(
         private router: Router,
         private activeRoute: ActivatedRoute,
         private apiService: ApiService,
-        private titleService: Title
+        private titleService: Title,
+        // private paginator: MatPaginator
     ) {
         sessionStorage.setItem('AppTitle', 'Manage Company');
     }
@@ -57,7 +53,7 @@ export class CompanyGridComponent implements OnInit, OnDestroy {
     ngOnInit() {
         this.titleService.setTitle('ONE | Manage Company');
         this.getAllCompany();
-        this.dataSource.paginator = this.paginator;
+        // this.dataSource.paginator = this.paginator;
     }
 
 
